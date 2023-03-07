9910905b320fa869844ca552dd82371e38233379
Abhishek singh <99178812+Abhishek-singh996@users.noreply.github.com>
2023-03-07 21:12:41 +0530

    logging and exception

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     name='MLProjects',
     version='0.0.1',
     author='Abhishek Singh',
-    author_email='nitianabhishek996@gmail.com',
+    author_email='abhishek.singh@livnsense.com',
     packages=find_packages(),
     install_requires = get_requirements('requirements.txt')
 )
